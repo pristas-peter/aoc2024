2c6cafb942eeb9e2e8156019ec9802e1f034dafc
Peter Pristas <pristas.peter@gmail.com>
2024-12-01 09:53:13 +0100

    Add fmt

--- day1.test.ts
+++ day1.test.ts
@@ -1,34 +1,30 @@
 import { assertEquals } from "jsr:@std/assert";
-import {part1, part2} from "./day1.ts";
+import { part1, part2 } from "./day1.ts";
 
 Deno.test("part1 - test input", async () => {
-    const data = await Deno.readTextFile('./day1.test.txt');
-    const result = part1(data)
+  const data = await Deno.readTextFile("./day1.test.txt");
+  const result = part1(data);
 
-
-    assertEquals(result, 11);
+  assertEquals(result, 11);
 });
 
 Deno.test("part1 - input", async () => {
-    const data = await Deno.readTextFile('./day1.txt');
-    const result = part1(data)
+  const data = await Deno.readTextFile("./day1.txt");
+  const result = part1(data);
 
-
-    assertEquals(result, 2430334);
+  assertEquals(result, 2430334);
 });
 
 Deno.test("part2 - test input", async () => {
-    const data = await Deno.readTextFile('./day1.test.txt');
-    const result = part2(data)
+  const data = await Deno.readTextFile("./day1.test.txt");
+  const result = part2(data);
 
-
-    assertEquals(result, 31);
+  assertEquals(result, 31);
 });
 
 Deno.test("part2 - input", async () => {
-    const data = await Deno.readTextFile('./day1.txt');
-    const result = part2(data)
+  const data = await Deno.readTextFile("./day1.txt");
+  const result = part2(data);
 
-
-    assertEquals(result, 28786472);
+  assertEquals(result, 28786472);
 });
--- day1.ts
+++ day1.ts
@@ -1,55 +1,50 @@
-
 function parse(input: string) {
-    const a: number[] = []
-    const b: number[] = []
+  const a: number[] = [];
+  const b: number[] = [];
 
-    input.split('\n').forEach(line => {
+  input.split("\n").forEach((line) => {
+    const match = line.match(/^(\d+) +(\d+)$/);
 
-        const match = line.match(/^(\d+) +(\d+)$/)
+    a.push(Number(match![1]));
+    b.push(Number(match![2]));
+  });
 
-        a.push(Number(match![1]))
-        b.push(Number(match![2]))
-    })
-
-
-    return [a, b]
-
+  return [a, b];
 }
 
-
 export function part1(input: string) {
-    const [a, b] = parse(input)
+  const [a, b] = parse(input);
 
-    const aSorted = a.toSorted()
-    const bSorted = b.toSorted()
+  const aSorted = a.toSorted();
+  const bSorted = b.toSorted();
 
+  const distances: number[] = [];
 
-    const distances: number[] = []
+  for (let i = 0; i < aSorted.length; i++) {
+    distances.push(Math.abs(aSorted[i] - bSorted[i]));
+  }
 
-    for (let i = 0; i < aSorted.length; i++) {
-        distances.push(Math.abs(aSorted[i] - bSorted[i]))
-    }
-
-    return distances.reduce((sum, i) => { return sum + i}, 0 )
-
+  return distances.reduce((sum, i) => {
+    return sum + i;
+  }, 0);
 }
 
 export function part2(input: string) {
-    const [a, b] = parse(input)
+  const [a, b] = parse(input);
 
-    const similarity: number[] = []
+  const similarity: number[] = [];
 
-    const bCounts: Record<number, number> = {}
+  const bCounts: Record<number, number> = {};
 
-    b.forEach(num => {
-        bCounts[num] = (bCounts[num] ?? 0)+ 1
-    })
+  b.forEach((num) => {
+    bCounts[num] = (bCounts[num] ?? 0) + 1;
+  });
 
-    a.forEach((num, index) => {
-        similarity[index] = (bCounts[num] ?? 0) * num
-    })
+  a.forEach((num, index) => {
+    similarity[index] = (bCounts[num] ?? 0) * num;
+  });
 
-    return similarity.reduce((sum, i) => { return sum + i}, 0 )
-
-
+  return similarity.reduce((sum, i) => {
+    return sum + i;
+  }, 0);
 }
